51aa31ec27e5c62d838cbdf87d72e9b9c70f2254
zhangbl <zhangbl@guotailimin.com>
2020-09-08 17:02:16 +0800

    update config

--- .umirc.ts
+++ .umirc.ts
@@ -1,4 +1,37 @@
+import path from "path";
 import { defineConfig } from "umi";
+
+import packageJson from "./package.json";
+
+interface ExternalsItem {
+  key: string;
+  var: string;
+  cdn: string;
+}
+
+const NODE_ENV = process.env.NODE_ENV;
+const dependencies = packageJson.dependencies as Record<string, string>;
+
+const externals: ExternalsItem[] = [
+  {
+    key: "react",
+    var: "window.React",
+    cdn: `https://cdn.jsdelivr.net/npm/react@{version}/umd/react.${NODE_ENV}.min.js`,
+  },
+  {
+    key: "react-dom",
+    var: "window.ReactDOM",
+    cdn: `https://cdn.jsdelivr.net/npm/react-dom@{version}/umd/react-dom.${NODE_ENV}.min.js`,
+  },
+];
+
+// * 适用 package.json dependencies 版本号使用 ^version/version 格式
+const matchVersion = (it: ExternalsItem) => /^\^?\d+/.test(dependencies[it.key]);
+
+const matchExternals = externals.filter(matchVersion).map(it => ({
+  ...it,
+  cdn: it.cdn.replace("{version}", dependencies[it.key].replace(/^\^/, "")),
+}));
 
 export default defineConfig({
   base: "/",
@@ -10,6 +43,7 @@ export default defineConfig({
   define: {
     ENV: process.env.NODE_ENV,
   },
+  proxy: {},
   targets: {
     chrome: 79,
     firefox: false,
@@ -17,11 +51,40 @@ export default defineConfig({
     edge: false,
     ios: false,
   },
-  esbuild: {},
-  nodeModulesTransform: {
-    type: "none",
-  },
   cssLoader: {
     localsConvention: "camelCase",
   },
+  ignoreMomentLocale: true,
+  esbuild: {},
+  nodeModulesTransform: {
+    type: "none",
+    exclude: [],
+  },
+  externals: matchExternals.reduce(
+    (prev, curr) =>
+      Object.assign(prev, {
+        [curr.key]: curr.var,
+      }),
+    {} as Record<string, string>,
+  ),
+  scripts: matchExternals.map(it => it.cdn),
+  chainWebpack: config => {
+    config.resolve.alias.set("@", path.resolve(__dirname, "./src/"));
+    config.optimization.splitChunks({
+      chunks: "all",
+      automaticNameDelimiter: ".",
+      name: true,
+      minSize: 30000,
+      minChunks: 1,
+      cacheGroups: {
+        vendors: {
+          name: "vendors",
+          test: /[\\/]node_modules[\\/]/,
+          priority: -11,
+          enforce: true,
+        },
+      },
+    });
+  },
+  chunks: ["vendors", "umi"],
 });
