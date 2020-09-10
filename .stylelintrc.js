fce04ddaf32ba8443d1aeb865ef8d3fe89599a85
zhangbl <zhangbl@guotailimin.com>
2020-09-10 17:00:06 +0800

    add stylelint

---
+++ .stylelintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  defaultSeverity: "error",
+  extends: ["stylelint-config-prettier", "stylelint-config-idiomatic-order"],
+  rules: {
+    "color-hex-length": "short",
+  },
+};
